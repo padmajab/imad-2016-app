2ca1da8bed85212ffb8f5faee07606d14cfe6948
padmajab <padmajaapr20@gmail.com>
2016-09-21 23:02:19 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,51 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var articleone ={
+    title: 'article-one',
+    heading:'article one',
+    date:'sep 5 2016',
+    content:`
+            <P>  THIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLETHIS IS MY FIRST ARTICLE
+            </P>`
+};
+function createTemplate(data)
+{
+    var title= data.title;
+    var heading=data.heading;
+    var content=data.content;
+    var date=data.date;
+var htmltemplate=`
+<html>
+<head>
+    <title> ${title}</title>
+    <meta name='vimport' content="width-device-width, init[0]-scale-1"/>
+<link href="/ui/style.css" rel="stylesheet" />
 
+</head>
+<BODY>
+    <div class="container">
+    <DIV>
+        <A HREF='/'>HOME</A>
+        <HR/>
+    </DIV>
+    <H3>${heading} </H3>
+    
+    <DIV>
+        ${content}
+    </DIV>
+    <DIV>
+        <P> ${date}
+        </P>
+    </DIV>
+    </div>
+</BODY>
+</html>
+`;
+return htmltemplate;
+}
 app.get('/article-one', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));    
+res.send(createTemplate(articleone));    
 });
 app.get('/article-two', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));    
