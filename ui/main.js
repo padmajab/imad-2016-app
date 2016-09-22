dc616d216ef4dc90ab829d64631cc352aec2aded
padmajab <padmajaapr20@gmail.com>
2016-09-22 03:07:52 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
 console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML='new value';
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight()
+{
+    moveleft=moveleft + 10;
+    img.style.marginleft=marginleft + 'px';
+    }
+    img.onclick=function()
+    {
+        var interval=setInterval(moveRight,100);
+    }
